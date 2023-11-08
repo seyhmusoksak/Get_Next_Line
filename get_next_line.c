1aaf0d30502ce25dee78740c90da0101525de02e
seyhmus <seyhmusoksak@gmail.com>
2023-11-08 18:55:29 +0300

    fixed

--- get_next_line.c
+++ get_next_line.c
@@ -6,54 +6,110 @@
 /*   By: soksak <soksak@42istanbul.com.tr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 19:48:49 by soksak            #+#    #+#             */
-/*   Updated: 2023/11/01 22:55:40 by soksak           ###   ########.fr       */
+/*   Updated: 2023/11/08 18:34:15 by soksak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
-char *get_next_line(int fd)
+char	*take_line(char *buffer)
 {
-	char			buff[BUFFER_SIZE];
-	char			*line;
-	static ssize_t	fileread;
-	size_t			save;
-
-	while(fileread = read(fd, buff, BUFFER_SIZE - 1) > 0)
-	{
-		
-	}
-
-
-}
-
-int ft_checkline(char *str)
-{
+	char	*line;
 	size_t	i;
 
+	if (buffer[0] == 0)
+		return (NULL);
 	i = 0;
-	while(str[i])
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	line = (char *)malloc(sizeof(char) * i + 2);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
 	{
-		if (str[i] == '\n')
-			return(i);
+		line[i] = buffer[i];
 		i++;
 	}
-	return(i);
+	line[i] = '\n';
+	line[i + 1] = '\0';
+	return (line);
 }
 
-
-
-
-
-int main()
+char	*create_line(int fd, char *buffer)
 {
-	int fd = open("dosya.txt", O_CREAT | O_RDONLY, 0777);
+	int		readbyte;
+	char	*temp;
 
-	int i = 0;
-	while (i <= 5)
+	readbyte = 1;
+	temp = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!temp)
+		return (NULL);
+	while (!ft_strchr(buffer, '\n') && readbyte)
 	{
-		get_next_line(fd);
+		readbyte = read(fd, temp, BUFFER_SIZE);
+		if (readbyte == -1)
+		{
+			free(temp);
+			free(buffer);
+			return (NULL);
+		}
+		temp[readbyte] = '\0';
+		buffer = ft_strjoin(buffer, temp);
+	}
+	free(temp);
+	return (buffer);
+}
+
+char	*remove_line(char *buffer, size_t len)
+{
+	size_t	i;
+	char	*newline;
+
+	if (buffer[len] == 0)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	newline = (char *)malloc(sizeof(char) * (ft_strlen(buffer) - len + 1));
+	if (!newline)
+		return (NULL);
+	i = 0;
+	while (buffer[len])
+		newline[i++] = buffer[len++];
+	newline[i] = '\0';
+	free(buffer);
+	return (newline);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = create_line(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	line = take_line(buffer);
+	buffer = remove_line(buffer, ft_strlen(line));
+	return (line);
+}
+
+int	main(void)
+{
+	int		fd;
+	char	*s;
+	int		i;
+
+	fd = open("test.txt", O_CREAT | O_RDONLY, 0777);
+	i = 0;
+	while (i < 165)
+	{
+		s = get_next_line(fd);
+		printf("%s", s);
+		free(s);
+		i++;
 	}
 }
-
--- get_next_line.h
+++ get_next_line.h
@@ -6,22 +6,30 @@
 /*   By: soksak <soksak@42istanbul.com.tr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 19:40:04 by soksak            #+#    #+#             */
-/*   Updated: 2023/11/01 22:27:11 by soksak           ###   ########.fr       */
+/*   Updated: 2023/11/08 18:39:10 by soksak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 10
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000000
+# endif
 
 # include <fcntl.h>
 # include <stdio.h>
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
 
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*create_line(int fd, char *buffer);
+char	*remove_line(char *buffer, size_t len);
+char	*take_line(char *buffer);
 
-char *get_next_line(int fd);
-
-
+char	*ft_strnstr(const char *str, const char *to_find, size_t len);
 
 #endif
